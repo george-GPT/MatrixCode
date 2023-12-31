522562121f5852b30b6460cf8a0e3caa80db5626
george-GPT <Georgedewart21@gmail.com>
2023-12-30 23:02:53 -0500

    minor changes

--- script.js
+++ script.js
@@ -2,6 +2,46 @@ document.addEventListener('DOMContentLoaded', function () {
     // Fetch CSV data
     fetchData();
 
+    // Add event listener to the list-group div
+    const listGroup = document.querySelector('.list-group');
+    
+    if (listGroup) {
+        listGroup.addEventListener('click', function(event) {
+            // Check if the clicked element is a link
+            if (event.target.tagName === 'A') {
+                // Prevent default anchor click behavior
+                event.preventDefault();
+
+                // Get the target section's ID (e.g., #htmlCheatSheet)
+                var target = event.target.hash;
+
+                // Get the target element
+                var targetElement = document.querySelector(target);
+                if (targetElement) {
+                    // Calculate the target's position relative to the viewport
+                    var targetOffset = targetElement.getBoundingClientRect().top;
+
+                    // Calculate the current scroll position
+                    var scrollOffset = window.pageYOffset || document.documentElement.scrollTop;
+
+                    // Calculate the final scroll position
+                    var finalOffset = scrollOffset + targetOffset;
+
+                    // Use smooth scrolling using the scrollTo() method
+                    window.scrollTo({
+                        top: finalOffset,
+                        behavior: 'smooth'
+                    });
+                }
+            }
+        });
+    }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Fetch CSV data
+    fetchData();
+
     // Add event listeners for the cards if they exist
     const htmlCard = document.getElementById('html-card');
     if (htmlCard) {
@@ -55,3 +95,28 @@ function openPage(pageName) {
         fetchData();
     }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Fetch CSV data
+    fetchData();
+
+    // Add event listeners for all items within the list-group
+    const listGroupItems = document.querySelectorAll('.list-group a');
+    
+    listGroupItems.forEach(function(item) {
+        item.addEventListener('click', function(event) {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Get the target section's ID (e.g., #htmlCheatSheet)
+            var target = this.hash;
+
+            // Use jQuery's animate() function to smoothly scroll to the target
+            $('html, body').animate({
+                scrollTop: $(target).offset().top
+            }, 800); // 800 milliseconds for the scroll animation duration
+        });
+    });
+
+    // Rest of your code...
+});
