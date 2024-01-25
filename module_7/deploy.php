a150bfdace2e46d365f708e91c16920402a0d369
George <37326256+george-GPT@users.noreply.github.com>
2024-01-25 12:10:00 -0500

    minor fixes

--- module_7/deploy.php
+++ module_7/deploy.php
@@ -1,9 +1,9 @@
 <?php
 // Secret key to authenticate the webhook
-$secret = "your-secret-token";
+$secret = "x5PFxsi@@F+EL[p.Ypr]I5(W7jg(TL&]";
 
 // Path to the repository on your server
-$path = "/home/username/public_html";
+$path = "http://www.matrixcode.ca/module_4/deploy.php";
 
 // Headers deliver the payload hash
 $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
@@ -17,9 +17,5 @@ if ($hash === hash_hmac($algo, $payload, $secret)) {
     // Execute the commands to pull from the repository
     exec("cd {$path} && git pull");
     echo "Success";
-} else {
-    // Respond with a failure if the signature doesn't match
-    http_response_code(403);
-    echo "Forbidden";
-}
+} 
 ?>
